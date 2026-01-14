3a7c79d674b6335f264098900153ea335f2d1f7a
Veshal kumar singh <veshalkumar25@gmail.com>
2026-01-14 23:09:43 +0530

    Inline>Internal>External CSS

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,18 +1,38 @@
 import React from 'react'
+
+// importing external css
+import './Product.css'
+
+
 // Props
 const Product = ({ name, price, color, Person = {} }) => {
-    return (
 
-        <div>
+    // Internal CSS-Object
+    const mystyle = {
+        backgroundColor: 'white',
+        color: 'red',
+        padding: '10px',
+        margin: '10px',
+        border: '1px solid yellow'
+    }
+    return (
+        // Inline Styling - CSS
+        <div
+            // style={{ backgroundColor: 'red', padding: '10px', margin: '10px', border: '1px solid yellow', borderRadius: '10px' }}
+            // style={mystyle}
+            // <!--------- External CSS -------------------->
+            // className='container'
+            className='con-ext'
+        >
             <h1>Brand - {name}</h1>
             <h2>Price - {price}</h2>
             <h3>Price - {color}</h3>
-            {Person.name && (
+            {/* {Person.name && (
                 <>
                 <p>Name = {Person.name}</p>
                 <p>Age = {Person.age}</p>
             </>
-            )}
+            )} */}
         </div>
 
     )
